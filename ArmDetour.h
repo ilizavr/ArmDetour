74639c65758d5ddc7fcc3c80d7a1112db3684565
Oifox <77205519+Oifox@users.noreply.github.com>
2023-11-23 14:35:31 +0100

    Code optimization
    Added smart pointers to manage resources and prevent memory leaks. Error handling has also been added to improve reliability and security of code. Code readability has been improved.

--- ArmDetour.h
+++ ArmDetour.h
@@ -1,39 +1,70 @@
-#include <stdlib.h>
+#include <cstdlib>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <cstdint>
+#include <memory>
+#include <stdexcept>
 
-#define _SYS_PAGE_SIZE_ (sysconf(_SC_PAGE_SIZE))
-#define _PAGE_START_OF_(x)    ((uintptr_t)x & ~(uintptr_t)(_SYS_PAGE_SIZE_ - 1))
-#define _PAGE_END_OF_(x, len) (_PAGE_START_OF_((uintptr_t)x + len - 1))
-#define _PAGE_LEN_OF_(x, len) (_PAGE_END_OF_(x, len) - _PAGE_START_OF_(x) + _SYS_PAGE_SIZE_)
-#define _PAGE_OFFSET_OF_(x)   ((uintptr_t)x - _PAGE_START_OF_(x))
+namespace ilizavr {
 
-
-namespace ilizavr{
-	uintptr_t ArmDetour(uintptr_t addresssrc,uintptr_t function){
-		//simple arm hook function by ilizavr. t.me/pinkdevil222
-		
-		//mapping region for oldfunc
-		uintptr_t oldvalue=(uintptr_t)mmap(NULL,1024,(PROT_READ|PROT_WRITE|PROT_EXEC),(MAP_PRIVATE|MAP_ANONYMOUS),-1,NULL);
-		//copy 8 bytes from orig func to old func
-		*(long long *)(oldvalue) = *(long long *)(addresssrc);
-		//trampoline to orig func
-		*(unsigned char *)(oldvalue+8)=4;
-		*(unsigned char *)(oldvalue+9)=0xF0;
-		*(unsigned char *)(oldvalue+10)=0x1F;
-		*(unsigned char *)(oldvalue+11)=0xE5;
-		*(uintptr_t *)(oldvalue+0xC)=addresssrc+8;
-		
-		//change protect of orig func
-		mprotect((void *)_PAGE_START_OF_(addresssrc), _PAGE_LEN_OF_(addresssrc, 8), (PROT_READ|PROT_WRITE|PROT_EXEC));
-		//trampoline to hooked func
-		*(unsigned char *)(addresssrc+0)=4;
-		*(unsigned char *)(addresssrc+1)=0xF0;
-		*(unsigned char *)(addresssrc+2)=0x1F;
-		*(unsigned char *)(addresssrc+3)=0xE5;
-		*(uintptr_t *)(addresssrc+4)=function;
-		
-		//return old func
-		return oldvalue;
+class ArmHook {
+public:
+    static uintptr_t HookFunction(uintptr_t target, uintptr_t newFunction) {
+        auto oldFunction = CreateTrampoline(target);
+        if (!oldFunction) {
+            throw std::runtime_error("Failed to create trampoline for the function.");
 	}
+        if (ChangeProtection(target) != 0) {
+            throw std::runtime_error("Failed to change memory protection.");
+	}
+        WriteJump(target, newFunction);
+        return reinterpret_cast<uintptr_t>(oldFunction.get());
+    }
+
+private:
+    static std::unique_ptr<uint8_t[]> CreateTrampoline(uintptr_t target) {
+        const size_t trampolineSize = 16;
+        auto trampoline = std::make_unique<uint8_t[]>(trampolineSize);
+        if (trampoline == nullptr) {
+            throw std::bad_alloc();
+        }
+
+        std::memcpy(trampoline.get(), reinterpret_cast<void*>(target), 8);
+
+        // write jump from trampoline to remaining part of original function
+        trampoline[8] = 0x04;
+        trampoline[9] = 0xF0;
+        trampoline[10] = 0x1F;
+        trampoline[11] = 0xE5;
+        *reinterpret_cast<uintptr_t*>(&trampoline[12]) = target + 8;
+        return trampoline;
+    }
+
+    static int ChangeProtection(uintptr_t target) {
+        uintptr_t start = _PageStartOf(target);
+        size_t length = _PageLength(target, 8);
+        return mprotect(reinterpret_cast<void*>(start), length, PROT_READ | PROT_WRITE | PROT_EXEC);
+    }
+
+    static void WriteJump(uintptr_t target, uintptr_t destination) {
+        // writing jump instruction to new function
+        auto* targetPtr = reinterpret_cast<uint8_t*>(target);
+        targetPtr[0] = 0x04;
+        targetPtr[1] = 0xF0;
+        targetPtr[2] = 0x1F;
+        targetPtr[3] = 0xE5;
+        *reinterpret_cast<uintptr_t*>(&targetPtr[4]) = destination;
+    }
+
+    static uintptr_t _PageStartOf(uintptr_t address) {
+        return address & ~(static_cast<uintptr_t>(sysconf(_SC_PAGE_SIZE)) - 1);
+    }
+
+    static size_t _PageLength(uintptr_t address, size_t length) {
+        uintptr_t start = _PageStartOf(address);
+        uintptr_t end = _PageStartOf(address + length - 1);
+        return end - start + sysconf(_SC_PAGE_SIZE);
+    }
+};
+
 }
